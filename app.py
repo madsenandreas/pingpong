49f8c3423c4f4be43ea677687d62bb7e90f5f33c
Andreas Madsen <andreasmadsen85@gmail.com>
2024-11-11 12:44:51 +0100

    Add local jquery

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ TEMPLATE = """
     <div class="half black-half" id="blackTeam">
         BlackSide: <span id="count_black">{{ black }}</span>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./jquery.min.js"></script>
     <script>
         $(document).ready(function() {
             function fetchCount_white() {
