b0df7e4f7bc8ef85388e865f04b5e88656195c82
Andreas Madsen <andreasmadsen85@gmail.com>
2024-11-11 13:37:12 +0100

    Change startup procedure

--- start-firefox.sh
+++ start-firefox.sh
@@ -5,7 +5,8 @@ sleep 30
 #DISPLAY=:0 firefox -url http://127.0.0.1:5000/ &
 DISPLAY=:0 firefox &
 # Wait for Firefox to fully load
-sleep 10
+sleep 20
 # Use xdotool to simulate pressing F11 to enter fullscreen mode
-DISPLAY=:0 xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
-
+DISPLAY=:0 xdotool search --sync --onlyvisible --class "Firefox" windowactivate
+DISPLAY=:0 xdotool search --sync --onlyvisible --class "Firefox" windowfocus
+DISPLAY=:0 xdotool key F11
